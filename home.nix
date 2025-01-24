c8ce6032d9e860015594c0cf9a2a51e7a576bfcf
Kabir Mathur Chattopadhyay <kabir1703@gmail.com>
2025-01-24 14:16:48 -0800

    factor out zsh settings

--- home.nix
+++ home.nix
@@ -40,34 +40,5 @@
 
   programs.git = import ./git-settings.nix;
 
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
-    enableCompletion = true;
-    initExtra = ''
-      export NVM_DIR="$([ -z "$HOME/.config-" ] && printf %s "$HOME/.nvm" || printf %s "$HOME/.config/nvm")" 
-      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-      export PATH="/home/kabir/.local/bin:$PATH"
-    '';
-    history = {
-      expireDuplicatesFirst = true;
-      save = 100000000;
-      size = 1000000000;
-    };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "autojump"
-        "sudo"
-        "fd"
-        "git-flow"
-        "pyenv"
-        "nvm"
-        "yarn"
-        "poetry"
-      ];
-    };
-  };
+  programs.zsh = import ./zsh-settings.nix;
 }
