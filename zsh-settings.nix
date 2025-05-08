808ff00412578b266c401d3c5ab062ddc46db924
Kabir Mathur Chattopadhyay <kabir1703@gmail.com>
2025-05-07 20:01:04 -0700

    Fix pyenv

--- zsh-settings.nix
+++ zsh-settings.nix
@@ -5,9 +5,17 @@
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     initExtra = ''
+      # NVM
       export NVM_DIR="$([ -z "$HOME/.config-" ] && printf %s "$HOME/.nvm" || printf %s "$HOME/.config/nvm")" 
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
       export PATH="/home/kabir/.local/bin:$PATH"
+
+      # Pyenv
+      export PYENV_ROOT="$HOME/.pyenv"
+      [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+      eval "$(pyenv init --path)"
+      eval "$(pyenv init - zsh)"
+
     '';
     history = {
       expireDuplicatesFirst = true;
