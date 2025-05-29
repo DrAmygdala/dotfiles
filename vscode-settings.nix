75f01078fa9fde95206adb05dab0a7c23ff94293
Kabir Mathur Chattopadhyay <kabir1703@gmail.com>
2025-05-29 16:56:09 -0700

    Add some extensions to vscode

--- vscode-settings.nix
+++ vscode-settings.nix
@@ -6,16 +6,25 @@
 	package = pkgs.vscodium;
 	extensions = [
 		pkgs.vscode-extensions.bbenoist.nix
+		pkgs.vscode-extensions.bungcip.better-toml
+		pkgs.vscode-extensions.mikestead.dotenv
+		pkgs.vscode-extensions.grapecity.gc-excelviewer
+		pkgs.vscode-extensions.timonwong.shellcheck
+		pkgs.vscode-extensions.redhat.vscode-yaml
+		pkgs.vscode-extensions.ms-python.python
+		pkgs.vscode-extensions.ms-python.debugpy
+		pkgs.vscode-extensions.ms-python.vscode-pylance
+		pkgs.vscode-extensions.vscodevim.vim
 	];
 	mutableExtensionsDir = true;
 	userSettings = {
-	    "editor.renderWhitespace": "all",
-	    "python.languageServer": "Pylance",
-	    "files.autoSave": "afterDelay",
-	    "editor.formatOnSave": true,
-	    "svelte.enable-ts-plugin": true,
-	    "window.zoomLevel": -1,
-	    "editor.minimap.enabled": false,
-	    "autoDocstring.docstringFormat": "sphinx"
+	    "editor.renderWhitespace" = "all";
+	    "python.languageServer" = "Pylance";
+	    "files.autoSave" = "afterDelay";
+	    "editor.formatOnSave" = true;
+	    "svelte.enable-ts-plugin" = true;
+	    "window.zoomLevel" = -1;
+	    "editor.minimap.enabled" = false;
+	    "autoDocstring.docstringFormat" = "sphinx";
 	};
 }
