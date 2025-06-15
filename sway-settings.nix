d2088ab009ef91683ffe4b0d12508bad1c268dd2
Kabir Mathur Chattopadhyay <kabir1703@gmail.com>
2025-06-15 10:16:48 -0700

    Add fuzzel and modify some basic settings

--- sway-settings.nix
+++ sway-settings.nix
@@ -1,10 +1,20 @@
+{ pkgs }:
 {
   enable = true;
   config = rec {
     modifier = "Mod4";
     terminal = "alacritty"; 
+    menu = "${pkgs.fuzzel}/bin/fuzzel";
+    bars = [ ];
     startup = [
       {command = "alacritty";}
     ];
+    focus = {
+	followMouse = false;
+    };
+    window = {
+    	titlebar = false;
+    };
+    workspaceAutoBackAndForth = true;
   };
 }
