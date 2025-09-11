274346d1bfaa826f81e2edf91343a76960fcd02e
Kabir Mathur Chattopadhyay <kabir1703@gmail.com>
2025-09-11 11:13:00 -0700

    Enable hyprland instead of sway

--- home.nix
+++ home.nix
@@ -34,8 +34,9 @@
     (import ./common/zsh-packages.nix { inherit pkgs; })
   ];
 
-  wayland.windowManager.sway = (import ./common/sway-settings.nix { inherit pkgs; });
+  wayland.windowManager.hyprland = (import ./common/hyprland-settings.nix { inherit pkgs; });
   editorconfig = import ./common/editorconfig.nix;
+  services.hyprpaper = import ./common/hyprpaper-settings.nix;
 
   programs.waybar = (import ./common/waybar-settings.nix { inherit pkgs; });
   programs.starship = import ./common/starship-settings.nix;
