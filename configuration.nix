029b2b225e3771acd09a93a35509091b3d88c90a
Kabir <kabir1703@gmail.com>
2025-09-22 07:55:02 -0700

    Use UWSM to handle logout of hyprland more gracefully

--- configuration.nix
+++ configuration.nix
@@ -38,8 +38,11 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
@@ -79,6 +82,10 @@
   programs.firefox.enable = true;
 
   programs.zsh.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
