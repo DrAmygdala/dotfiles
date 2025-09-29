44643b1e4454c24eaf13c820aac8a3242e1ff87d
Kabir <kabir1703@gmail.com>
2025-09-28 20:27:00 -0700

    Use the hyprland git url for inputs to home manager as well

--- common/hyprland-settings.nix
+++ common/hyprland-settings.nix
@@ -1,6 +1,8 @@
-{ pkgs }:
+{ pkgs, inputs }:
 {
-	enable = true;
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     settings = {
         "$mod" = "SUPER";
         exec-once = [
