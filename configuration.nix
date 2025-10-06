5b8fc4c1ac64ebf11df0142ac018679e8a3917a1
Kabir <kabir1703@gmail.com>
2025-10-06 09:27:02 -0700

    Remove KDE Plasma

--- configuration.nix
+++ configuration.nix
@@ -54,8 +54,6 @@
     enable = true;
     wayland.enable = true;
   };
-  # Enable the KDE Plasma Desktop Environment.
-  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
