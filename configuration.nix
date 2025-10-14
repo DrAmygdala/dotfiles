2a921d9ba1468f5b2fcd0922dbc885388e0ec751
Kabir <kabir1703@gmail.com>
2025-10-14 13:50:01 -0700

    Add sway as wm option

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,7 @@
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
+  programs.sway.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
