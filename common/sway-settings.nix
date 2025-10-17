1a3e0073ce980d8109262b22eaa1b2f3277d6705
Kabir <kabir1703@gmail.com>
2025-10-17 14:55:55 -0700

    Add basic keybinds to sway

--- common/sway-settings.nix
+++ common/sway-settings.nix
@@ -1,9 +1,26 @@
-{ pkgs }:
+{ pkgs, lib }:
 {
   enable = true;
   systemd.enable = true;
   config = rec {
     modifier = "Mod4";
+    keybindings = lib.mkOptionDefault {
+       "CTRL+l" = "focus right";
+       "CTRL+k" = "focus up";
+       "CTRL+j" = "focus down";
+       "CTRL+h" = "focus left";
+       "CTRL+Mod4+l" = "workspace next";
+       "CTRL+Mod4+h" = "workspace prev";
+       "ALT+Space" = "exec ${pkgs.fuzzel}/bin/fuzzel";
+       "ALT+f" = "exec ${pkgs.firefox}/bin/firefox";
+       "ALT+s" = "exec ${pkgs.alacritty}/bin/alacritty";
+       "ALT+q" = "kill";
+       "XF86MonBrightnessUp" = "exec brightnessctl set 5%+";
+       "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
+       "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+       "XF86AudioRaiseVolume" = "exec wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+";
+       "XF86AudioLowerVolume" = "exec wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%-";
+    };
     terminal = "alacritty";
     menu = "${pkgs.fuzzel}/bin/fuzzel";
     output = {
@@ -16,7 +33,7 @@
       {command = "alacritty";}
     ];
     focus = {
-	followMouse = false;
+	    followMouse = false;
     };
     window = {
     	titlebar = false;
--- home.nix
+++ home.nix
@@ -37,7 +37,7 @@
   gtk = (import ./common/gtk-settings.nix { inherit pkgs; });
   xdg = import ./common/xdg-settings.nix;
 
-  wayland.windowManager.sway = (import ./common/sway-settings.nix { inherit pkgs; });
+  wayland.windowManager.sway = (import ./common/sway-settings.nix { inherit pkgs lib; });
   editorconfig = import ./common/editorconfig.nix;
   services.hyprpaper = import ./common/hyprpaper-settings.nix;
   services.swaync = import ./common/swaync-settings.nix;
