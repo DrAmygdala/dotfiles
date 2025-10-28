ba8043b4d142c8d51d7b099412624492a5a3b12f
Kabir <kabir1703@gmail.com>
2025-10-28 11:37:24 -0700

    Replace SSH default settings

--- common/ssh-settings.nix
+++ common/ssh-settings.nix
@@ -1,5 +1,20 @@
 {
     enable = true;
+    enableDefaultConfig = false;
+    matchBlocks = {
+        "*" = {
+            addKeysToAgent = "yes";
+            compression = true;
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+            forwardAgent = true;
+            hashKnownHosts = true;
+            serverAliveCountMax = 3;
+            serverAliveInterval = 0;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+        };
+    };
     includes = [
         "~/.ssh/config.d/config"
     ];
