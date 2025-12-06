be1d80bb3dc789f24d6e6ebd1c5f44c8f319c918
Kabir <kabir1703@gmail.com>
2025-12-05 21:36:35 -0800

    Add lutris

--- home.nix
+++ home.nix
@@ -87,4 +87,5 @@
   programs.tealdeer = import ./common/tealdeer-settings.nix;
   programs.ssh = import ./common/ssh-settings.nix;
   programs.swaylock = import ./common/swaylock-settings.nix;
+  programs.lutris = import ./common/lutris-settings.nix { inherit pkgs; };
 }
