73025e0f7c8c1d3675c56c18fd5bc84fde3908e3
Kabir <kabir1703@gmail.com>
2026-04-23 19:38:24 +0100

    automatically set timezone

--- configuration.nix
+++ configuration.nix
@@ -72,7 +72,7 @@
   # };
 
   # Set your time zone.
-  time.timeZone = "America/Vancouver";
+  services.automatic-timezoned.enable = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
