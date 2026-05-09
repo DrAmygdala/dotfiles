43613696a4835120512e908bcdc2a46512e9f087
Kabir <kabir1703@gmail.com>
2026-05-09 01:55:45 -0700

    Add anki

--- home/gui/anki.nix
+++ home/gui/anki.nix
@@ -1,6 +1,21 @@
+{ pkgs, ... }:
 {
   programs.anki = {
     enable = true;
+    addons = with pkgs.ankiAddons; [
+      review-heatmap
+      fsrs4anki-helper
+    ];
+    profiles.me = {
+      default = true;
+      sync = {
+        autoSync = true;
+        autoSyncMediaMinutes = 15;
+        keyFile = "/home/sourlemon/Downloads/anki-sync-key.txt";
+        syncMedia = true;
+        url = "https://anki.dnsaur.duckdns.org";
+      };
+    };
   };
   catppuccin.anki.enable = true;
 }
