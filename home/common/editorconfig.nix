e46c72dbc84aaea95551995a76c667f7e27bbf19
Kabir <kabir1703@gmail.com>
2026-05-18 08:39:13 +0100

    Make line length checks applicable to .md and .txt files only

--- home/common/editorconfig.nix
+++ home/common/editorconfig.nix
@@ -6,7 +6,6 @@
       end_of_line = "lf";
       trim_trailing_whitespace = true;
       insert_final_newline = true;
-      max_line_length = 80;
       indent_style = "space";
       indent_size = 4;
       spelling_language = "en-CA";
@@ -19,6 +18,12 @@
     };
     "*.yml" = {
       indent_size = 2;
+    };
+    "*.txt" = {
+      max_line_length = 80;
+    };
+    "*.md" = {
+      max_line_length = 80;
     };
   };
 }
